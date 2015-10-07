b89ad5b8d5414eabe75e329fc8d4a2ffe831ef15
psagan <patryk.sagan@icloud.com>
2015-10-07 13:31:50 +0200

    PESEL validator with tests

--- lib/nip_pesel_regon/validator/pesel.rb
+++ lib/nip_pesel_regon/validator/pesel.rb
@@ -1,8 +1,12 @@
 module NipPeselRegon
   module Validator
     class Pesel < Abstract
-
+      # last digit is 1 - it's not official weight but
+      # but it will help to calculate all checksum like: a+3b+7c+9d+e+3f+7g+9h+i+3j+k
+      # https://pl.wikipedia.org/wiki/PESEL
       WEIGHTS = [1, 3, 7, 9, 1, 3 ,7, 9, 1, 3, 1].freeze
+
+      PATTERN = /^\d{11}$/
 
       attr_reader :checksum_calculator
 
@@ -14,11 +18,27 @@ module NipPeselRegon
       private
 
       def validate
-        return false
+        # check if PESEL provided has proper format
+        return false unless has_proper_format?
+        calculate_sum % 10 == 0
+      end
+
+      # method responsible for detection if number provided has proper format
+      def has_proper_format?
+        !@number.nil? && matches_pattern?
+      end
+
+      # method responsible for pattern matching
+      def matches_pattern?
+        PATTERN =~ @number
+      end
+
+      def calculate_sum
+        @checksum_calculator.calculate
       end
 
       def normalize
-        @number
+        @number = @number.to_s.gsub(/[-\s]/, '')
       end
     end
   end
--- test/validator/pesel_test.rb
+++ test/validator/pesel_test.rb
@@ -1,10 +1,17 @@
 require 'test_helper'
 
 # @todo - check if add namespace here
+# All PESEL numbers are randomly generated in online generator
 class PeselTest < Minitest::Test
 
-  def test_when_improper_fixnum_pesel
-    validator = NipPeselRegon::Validator::Pesel.new(12345678901)
+  def test_that_validator_returns_true_for_valid_pesel
+    validator = NipPeselRegon::Validator::Pesel.new('00291600815')
+    assert validator.valid?
+  end
+
+
+  def test_that_validator_returns_false_for_invalid_pesel
+    validator = NipPeselRegon::Validator::Pesel.new('12345678901')
     refute validator.valid?
   end
 
