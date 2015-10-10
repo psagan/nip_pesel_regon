e6a5a95c6baf28e67a34def9457b368e829756c4
psagan <patryk.sagan@icloud.com>
2015-10-10 10:06:56 +0200

    Rails integration test for NIP - initial test file

---
+++ test/integration/rails/nip_test.rb
@@ -0,0 +1,23 @@
+require 'test_helper'
+require 'supermodel'
+require "nip_pesel_regon/integration/rails"
+
+class Company < SuperModel::Base
+  extend ActiveModel::Validations::HelperMethods
+  validates_nip_of :nip
+end
+
+# @todo - check if add namespace here
+class NipTest < Minitest::Test
+
+  def test_that_nip_validates_properly_in_model
+    c = Company.new(nip: '5882247715')
+    assert c.valid?
+  end
+
+  def test_that_improper_nip_do_not_validate_in_model
+    c = Company.new(nip: '5882247716')
+    refute c.valid?
+  end
+
+end
