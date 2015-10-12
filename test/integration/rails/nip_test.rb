cc744deea87284e506b9390ddff237f6c12dfc8e
psagan <patryk.sagan@icloud.com>
2015-10-12 10:14:44 +0200

    NIP Rails integration tests fixed

--- test/integration/rails/nip_test.rb
+++ test/integration/rails/nip_test.rb
@@ -7,9 +7,21 @@ module Test
   module Integration
     module Rails
       module Nip
-        class Company < SuperModel::Base
+
+        class CompanyBase < SuperModel::Base
           extend ActiveModel::Validations::HelperMethods
+        end
+
+        class Company < CompanyBase
           validates_nip_of :nip
+        end
+
+        class CompanyWithStrictNipTrue < CompanyBase
+          validates_nip_of :nip, strict: true
+        end
+
+        class CompanyWithStrictNipFalse < CompanyBase
+          validates_nip_of :nip, strict: false
         end
 
         class NipTest < Minitest::Test
@@ -22,6 +34,17 @@ module Test
           def test_that_improper_nip_do_not_validate_in_model
             c = Company.new(nip: '5882247716')
             refute c.valid?
+          end
+
+          def test_that_proper_formatted_nip_do_not_validate_in_model_when_strict
+            Company.validates_nip_of :nip, strict: true
+            c = CompanyWithStrictNipTrue.new(nip: '588-224-77-15')
+            refute c.valid?
+          end
+
+          def test_that_proper_formatted_nip_validates_in_model_when_strict_set_to_false
+            c = CompanyWithStrictNipFalse.new(nip: '588-224-77-15')
+            assert c.valid?
           end
 
         end
