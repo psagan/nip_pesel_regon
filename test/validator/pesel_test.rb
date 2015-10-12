db585f5fd7db797483fb843578f5832b98522922
psagan <patryk.sagan@icloud.com>
2015-10-12 21:27:09 +0200

    Additional unit tests for PESEL

--- test/validator/pesel_test.rb
+++ test/validator/pesel_test.rb
@@ -15,4 +15,50 @@ class PeselTest < Minitest::Test
     refute validator.valid?
   end
 
+  def test_when_proper_pesel_with_whitespaces
+    validator = NipPeselRegon::Validator::Pesel.new(' 00 29 16 00 815 ')
+    assert validator.valid?
+  end
+
+  def test_when_proper_pesel_with_dashes
+    validator = NipPeselRegon::Validator::Pesel.new('00-29-16-00-815')
+    assert validator.valid?
+  end
+
+  def test_that_improper_characters_prevent_validation
+    validator = NipPeselRegon::Validator::Pesel.new('00-29-16-00_815')
+    refute validator.valid?
+  end
+
+  def test_that_proper_pesel_validates_when_strict
+    validator = NipPeselRegon::Validator::Pesel.new('00291600815', strict: true)
+    assert validator.valid?
+  end
+
+  def test_that_improperly_formatted_pesel_do_not_validate_when_strict
+    validator = NipPeselRegon::Validator::Pesel.new('00-29-16-00-815', strict: true)
+    refute validator.valid?
+  end
+
+  def test_get_original_num
+    num = '00-29-16-00-815'
+    validator = NipPeselRegon::Validator::Pesel.new(num)
+    validator.valid?
+    assert_equal num, validator.original_number
+  end
+
+  def test_get_normalized_num
+    num = '00-29-16-00-815'
+    validator = NipPeselRegon::Validator::Pesel.new(num)
+    validator.valid?
+    assert_equal '00291600815', validator.number
+  end
+
+  def test_that_to_string_conversion_returns_normalized_num
+    num = '00-29-16-00-815'
+    validator = NipPeselRegon::Validator::Pesel.new(num)
+    validator.valid?
+    assert_equal '00291600815', validator.to_s
+  end
+
 end
