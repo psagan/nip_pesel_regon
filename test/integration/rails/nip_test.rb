c16b1326729df09b94a071a4ce30c360c564e4d9
psagan <patryk.sagan@icloud.com>
2015-10-12 19:36:03 +0200

    Change of NIP number used in tests

--- test/integration/rails/nip_test.rb
+++ test/integration/rails/nip_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 require 'supermodel'
 require "nip_pesel_regon/integration/rails"
 
-
+# # All NIP numbers are randomly generated in online generator
 module Test
   module Integration
     module Rails
@@ -28,40 +28,56 @@ module Test
           validates_nip_of :nip, message: 'test msg'
         end
 
+        class CompanyWithRawNip < CompanyBase
+          validates_nip_of :nip, save_normalized: false
+        end
+
         class NipTest < Minitest::Test
 
           def test_that_nip_validates_properly_in_model
-            c = Company.new(nip: '5882247715')
+            c = Company.new(nip: '1464791822')
             assert c.valid?
           end
 
           def test_that_improper_nip_do_not_validate_in_model
-            c = Company.new(nip: '5882247716')
+            c = Company.new(nip: '1464791824')
             refute c.valid?
           end
 
           def test_that_proper_formatted_nip_do_not_validate_in_model_when_strict
-            c = CompanyWithStrictNipTrue.new(nip: '588-224-77-15')
+            c = CompanyWithStrictNipTrue.new(nip: '146-479-18-22')
             refute c.valid?
           end
 
           def test_that_proper_formatted_nip_validates_in_model_when_strict_set_to_false
-            c = CompanyWithStrictNipFalse.new(nip: '588-224-77-15')
+            c = CompanyWithStrictNipFalse.new(nip: '146-479-18-22')
             assert c.valid?
           end
 
           def test_that_validation_of_not_proper_nip_adds_default_error_message
-            c = Company.new(nip: '5882247716')
+            c = Company.new(nip: '1464791824')
             refute c.valid?
             assert_equal 1 , c.errors[:nip].size
             assert_equal 'invalid NIP', c.errors[:nip].first
           end
 
           def test_that_validation_of_not_proper_nip_adds_custom_error_message
-            c = CompanyWithCustomMessage.new(nip: '5882247716')
+            c = CompanyWithCustomMessage.new(nip: '1464791824')
             refute c.valid?
             assert_equal 1 , c.errors[:nip].size
             assert_equal 'test msg', c.errors[:nip].first
+          end
+
+          def test_that_save_model_saves_normalized_number_by_default
+            c = Company.new(nip: '146-479-18-22')
+            c.save!
+            assert_equal '1464791822', c.nip
+          end
+
+          def test_that_save_model_saves_raw_number_when_proper_options_set
+            c = CompanyWithRawNip.new(nip: '146-479-18-22')
+            c.save!
+            assert_equal '146-479-18-22', c.nip
           end
 
         end
--- test/validator/nip_test.rb
+++ test/validator/nip_test.rb
@@ -1,49 +1,50 @@
 require 'test_helper'
 
 # @todo - check if add namespace here
+# # All NIP numbers are randomly generated in online generator
 class NipTest < Minitest::Test
 
   def test_when_proper_fixnum_nip
-    validator = NipPeselRegon::Validator::Nip.new(5882247715)
+    validator = NipPeselRegon::Validator::Nip.new(1464791822)
     assert validator.valid?
   end
 
   def test_when_improper_fixnum_nip
-    validator = NipPeselRegon::Validator::Nip.new(5882247716)
+    validator = NipPeselRegon::Validator::Nip.new(1464791824)
     refute validator.valid?
   end
 
   def test_when_proper_string_nip
-    validator = NipPeselRegon::Validator::Nip.new('5882247715')
+    validator = NipPeselRegon::Validator::Nip.new('1464791822')
     assert validator.valid?
   end
 
   def test_when_improper_string_nip
-    validator = NipPeselRegon::Validator::Nip.new('5882247716')
+    validator = NipPeselRegon::Validator::Nip.new('1464791824')
     refute validator.valid?
   end
 
   # @todo - more tests on different formats with PL, DE, EN ....
 
   def test_get_original_num
-    num = '588-224-77-15'
+    num = '146-479-18-22'
     validator = NipPeselRegon::Validator::Nip.new(num)
     validator.valid?
     assert_equal num, validator.original_number
   end
 
   def test_get_normalized_num
-    num = '588-224-77-15'
+    num = '146-479-18-22'
     validator = NipPeselRegon::Validator::Nip.new(num)
     validator.valid?
-    assert_equal '5882247715', validator.number
+    assert_equal '1464791822', validator.number
   end
 
   def test_that_to_string_conversion_returns_normalized_num
-    num = '588-224-77-15'
+    num = '146-479-18-22'
     validator = NipPeselRegon::Validator::Nip.new(num)
     validator.valid?
-    assert_equal '5882247715', validator.to_s
+    assert_equal '1464791822', validator.to_s
   end
 
 end
