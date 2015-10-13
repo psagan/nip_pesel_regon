bc6ac2d9342f37b40ea5f0e09ce1366a8325ee35
psagan <patryk.sagan@icloud.com>
2015-10-13 08:46:25 +0200

    Additional unit tests for REGON

--- test/validator/regon_test.rb
+++ test/validator/regon_test.rb
@@ -9,15 +9,35 @@ class RegonTest < Minitest::Test
     assert validator.valid?
   end
 
-  def test_when_proper_string_regon_with_whitespaces
-    regon = '1 2 3 4 5 6 7 8 5 1 2 3 4 7'
-    validator = NipPeselRegon::Validator::Regon.new(regon)
+  def test_improper_fixnum_regon
+    validator = NipPeselRegon::Validator::Regon.new(632188489)
+    refute validator.valid?
+  end
+
+  def test_that_proper_14_digit_regon_pass_validation
+    validator = NipPeselRegon::Validator::Regon.new('12345678512347')
     assert validator.valid?
   end
 
-  def test_improper_regon
-    validator = NipPeselRegon::Validator::Regon.new(632188489)
+  def test_that_improper_14_digit_regon_do_not_pass_validation
+    validator = NipPeselRegon::Validator::Regon.new('12345678512349')
     refute validator.valid?
+  end
+
+  def test_that_proper_9_digit_regon_pass_validation
+    validator = NipPeselRegon::Validator::Regon.new('632188483')
+    assert validator.valid?
+  end
+
+  def test_that_improper_9_digit_regon_do_not_pass_validation
+    validator = NipPeselRegon::Validator::Regon.new('632188485')
+    refute validator.valid?
+  end
+
+  def test_when_proper_string_regon_with_whitespaces
+    regon = ' 1 2 3 4 5 6 7 8 5 1 2 3 4 7 '
+    validator = NipPeselRegon::Validator::Regon.new(regon)
+    assert validator.valid?
   end
 
 end
