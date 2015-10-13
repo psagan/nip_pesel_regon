a337c26b26f7b635f8383ada6bd6df28898bdbe8
psagan <patryk.sagan@icloud.com>
2015-10-13 09:33:40 +0200

    Fix for classname in Regon9

--- test/validator/regon9_test.rb
+++ test/validator/regon9_test.rb
@@ -5,69 +5,69 @@ require 'test_helper'
 class Regon9Test < Minitest::Test
 
   def test_when_proper_fixnum_regon
-    validator = NipPeselRegon::Validator::Regon.new(632188483)
+    validator = NipPeselRegon::Validator::Regon9.new(632188483)
     assert validator.valid?
   end
 
   def test_improper_fixnum_regon
-    validator = NipPeselRegon::Validator::Regon.new(632188489)
+    validator = NipPeselRegon::Validator::Regon9.new(632188489)
     refute validator.valid?
   end
 
 
   def test_when_proper_string_regon
-    validator = NipPeselRegon::Validator::Regon.new('632188483')
+    validator = NipPeselRegon::Validator::Regon9.new('632188483')
     assert validator.valid?
   end
 
   def test_when_improper_string_regon
-    validator = NipPeselRegon::Validator::Regon.new('632188485')
+    validator = NipPeselRegon::Validator::Regon9.new('632188485')
     refute validator.valid?
   end
 
   def test_when_proper_string_regon_with_whitespaces
     regon = ' 6 3 2 1 8 8 4 8 3 '
-    validator = NipPeselRegon::Validator::Regon.new(regon)
+    validator = NipPeselRegon::Validator::Regon9.new(regon)
     assert validator.valid?
   end
 
   def test_when_proper_regon_with_dashes
-    validator = NipPeselRegon::Validator::Regon.new('632-188-483')
+    validator = NipPeselRegon::Validator::Regon9.new('632-188-483')
     assert validator.valid?
   end
 
   def test_that_improper_characters_prevent_validation
-    validator = NipPeselRegon::Validator::Regon.new('632-188_483')
+    validator = NipPeselRegon::Validator::Regon9.new('632-188_483')
     refute validator.valid?
   end
 
   def test_that_proper_regon_validates_when_strict
-    validator = NipPeselRegon::Validator::Regon.new('632188483')
+    validator = NipPeselRegon::Validator::Regon9.new('632188483')
     assert validator.valid?
   end
 
   def test_that_improperly_formatted_regon_do_not_validate_when_strict
-    validator = NipPeselRegon::Validator::Regon.new('632-188-483', strict: true)
+    validator = NipPeselRegon::Validator::Regon9.new('632-188-483', strict: true)
     refute validator.valid?
   end
 
   def test_get_original_num
     num = '632-188-483'
-    validator = NipPeselRegon::Validator::Regon.new(num)
+    validator = NipPeselRegon::Validator::Regon9.new(num)
     validator.valid?
     assert_equal num, validator.original_number
   end
 
   def test_get_normalized_num
     num = '632-188-483'
-    validator = NipPeselRegon::Validator::Regon.new(num)
+    validator = NipPeselRegon::Validator::Regon9.new(num)
     validator.valid?
     assert_equal '632188483', validator.number
   end
 
   def test_that_to_string_conversion_returns_normalized_num
     num = '632-188-483'
-    validator = NipPeselRegon::Validator::Regon.new(num)
+    validator = NipPeselRegon::Validator::Regon9.new(num)
     validator.valid?
     assert_equal '632188483', validator.to_s
   end
