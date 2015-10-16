cb9da83d25ec306d03a63c545965224f1e99cf3a
psagan <patryk.sagan@icloud.com>
2015-10-16 11:07:56 +0200

    Comments update in 14-digit REGON validator class.

--- lib/nip_pesel_regon/validator/regon14.rb
+++ lib/nip_pesel_regon/validator/regon14.rb
@@ -1,12 +1,18 @@
 module NipPeselRegon
   module Validator
+
+    # Class responsible for 14-digit REGON validation.
     class Regon14 < Base
+
+      # array with weights
       WEIGHTS = [2, 4, 8, 5, 0, 9, 7, 3, 6, 1, 2, 4, 8].freeze
 
+      # pattern for 14-digit REGON
       PATTERN = /^\d{14}$/
 
       private
 
+      # Main validation method.
       def validate
         # check if REGON with 14 digits is proper
         # REGON with 9 digits by slicing
@@ -18,9 +24,11 @@ module NipPeselRegon
         (checksum % 11) == number[-1].to_i
       end
 
-      # method responsible
+      # Method responsible
       # for testing if first 9 digits of 14-digit REGON
-      # are proper 9-digits regon (Regon9 class)
+      # are proper 9-digits regon (Regon9 class).
+      # There is strict dependency here as it's intended to not
+      # complicate gem's design.
       def is_valid_regon9?
         Regon9.new(number[0,9]).valid?
       end
