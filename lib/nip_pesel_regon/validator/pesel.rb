90af4f94fa4917d24710a92091edfc3e24dc5549
psagan <patryk.sagan@icloud.com>
2015-10-16 10:45:41 +0200

    Comments update in PESEL validator class.

--- lib/nip_pesel_regon/validator/pesel.rb
+++ lib/nip_pesel_regon/validator/pesel.rb
@@ -1,5 +1,8 @@
 module NipPeselRegon
   module Validator
+
+    # Class responsible for
+    # PESEL validation.
     class Pesel < Base
       # last digit is 1 - it's not official weight but
       # but it will help to calculate all checksum like: a+3b+7c+9d+e+3f+7g+9h+i+3j+k
@@ -7,10 +10,12 @@ module NipPeselRegon
       # pl - https://pl.wikipedia.org/wiki/PESEL
       WEIGHTS = [1, 3, 7, 9, 1, 3 ,7, 9, 1, 3, 1].freeze
 
+      # pattern for PESEL
       PATTERN = /^\d{11}$/
 
       private
 
+      # Main validation method.
       def validate
         checksum % 10 == 0
       end
