0ccac613f2fb23c8d540707bfccdd4d1e7fad722
psagan <patryk.sagan@icloud.com>
2015-10-16 11:05:54 +0200

    Comments update in 9-digit REGON validator class.

--- lib/nip_pesel_regon/validator/regon9.rb
+++ lib/nip_pesel_regon/validator/regon9.rb
@@ -1,12 +1,18 @@
 module NipPeselRegon
   module Validator
+
+    # Class responsible for 9-digit REGON validation.
     class Regon9 < Base
+
+      # array with weights
       WEIGHTS = [8, 9, 2, 3, 4, 5, 6, 7].freeze
 
+      # pattern for 9-digit REGON
       PATTERN = /^\d{9}$/
 
       private
 
+      # Main validation method.
       def validate
         mod = checksum % 11
 
