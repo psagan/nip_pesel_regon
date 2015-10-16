718d4b7ec55e04523e3e99d45ec09a2b490febfa
psagan <patryk.sagan@icloud.com>
2015-10-16 12:09:07 +0200

    Update comments in file responsible for Rails integration

--- lib/nip_pesel_regon/integration/rails.rb
+++ lib/nip_pesel_regon/integration/rails.rb
@@ -2,7 +2,8 @@ require 'active_support/inflector'
 
 module NipPeselRegon
   module Integration
-      # class responsible for simple integration
+
+      # Class responsible for simple integration
       # NIP/PESEL/REGON validation into active model validation
     class Rails < ActiveModel::EachValidator
 
@@ -17,8 +18,8 @@ module NipPeselRegon
         handle_default_options
       end
 
-      # method responsible for handling default options
-      # and merging them with custom options provided by developer
+      # Method responsible for handling default options
+      # and merging them with custom options provided by developer.
       def handle_default_options
         # before mergin check if custom :message provided by developer
         message_provided = options[:message]
@@ -30,7 +31,6 @@ module NipPeselRegon
         # custom message was provided by developer
         @options[:message] = options[:message] % options[:validator].upcase unless message_provided
       end
-
 
       def validate_each(record, attribute, value)
         # do validation
@@ -52,11 +52,27 @@ end
 
 module ActiveModel
   module Validations
+
+    # Standard Rails module which will
+    # be included into ActiveRecord::Base.
+    #
+    # Code below is responsible for NIP/PESEL/REGON
+    # validation methods creation.
+    #
+    # In the result subclasses of ActiveRecord::Base will
+    # have following methods available:
+    # - validates_nip_of
+    # - validates_pesel_of
+    # - validates_regon_of
+    #
     module HelperMethods
       %w{nip pesel regon}.each do |validator_name|
         define_method("validates_#{validator_name}_of") do |*attr_names|
+          # extract options to add validator name to them
           options = attr_names.extract_options!
+          # add validator name
           options[:validator] = validator_name
+          # add options back to attr_names
           attr_names << options
           validates_with NipPeselRegon::Integration::Rails, _merge_attributes(attr_names)
         end
