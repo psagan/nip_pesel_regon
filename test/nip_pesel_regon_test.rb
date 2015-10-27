b17cd0915b1de5cad399f679eabde5891c3b19dd
psagan <patryk.sagan@icloud.com>
2015-10-27 10:36:31 +0100

    Namespaces for Version Number tests

--- test/nip_pesel_regon_test.rb
+++ test/nip_pesel_regon_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
-class NipPeselRegonTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::NipPeselRegon::VERSION
+module Test
+  class NipPeselRegonTest < Minitest::Test
+
+    def test_that_it_has_a_version_number
+      refute_nil ::NipPeselRegon::VERSION
+    end
+
   end
 end
